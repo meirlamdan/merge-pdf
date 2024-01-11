e7b976b1d093483208ab787dbd68dfae54fbb214
meir lerner <mchlerner@gmail.com>
2024-01-11 22:29:02 +0200

    pdf preview

--- src/components/File.vue
+++ src/components/File.vue
@@ -1,25 +1,26 @@
 <script setup>
-import { mdiFilePdfBox } from "@mdi/js";
+import { onMounted, ref } from "vue";
+
+import pdfImg from '../pdfToImg.js'
 
 const props = defineProps(["file"]);
+const src = ref(null);
+
+onMounted(async () => {
+  src.value = await pdfImg(props.file);
+});
 </script>
 
 <template>
   <div class="file">
-    <span class="delete-file" @click.stop="$emit('deleteFile', file)">✖</span>
-    <div class="icon">
-      <Icon :path="mdiFilePdfBox" size="28" color="gray" />
-    </div>
-    <div>
-      <input
-        class="pages"
-        type="text"
-        v-model="file.pages"
-        placeholder="Select Pages"
-        @focus="$emit('inputFocus')"
-        @blur="$emit('inputBlur')"
-      />
-      {{ file.pages }}
+    <div class="file-view">
+      <img :src alt="" v-if="src">
+      <span class="delete-file" @click.stop="$emit('deleteFile', file)">✖</span>
+      <div class="select-pages">
+        <input type="text" v-model="file.pages" placeholder="Select Pages" @focus="$emit('inputFocus')"
+          @blur="$emit('inputBlur')" />
+        {{ file.pages }}
+      </div>
     </div>
     <div class="name">{{ file.name }}</div>
   </div>
@@ -27,44 +28,61 @@ const props = defineProps(["file"]);
 
 <style scoped>
 .file {
-  height: 150px;
-  width: 120px;
-  border: 1px solid #000;
-  border-radius: 5px;
-  position: relative;
-  margin: 5px;
+  margin: 10px;
+  width: 150px;
   background: #f5f5f5;
+  border-radius: 5px;
+  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
 }
+
+.file-view {
+  height: 180px;
+  padding: 7px 7px 0 7px;
+  position: relative;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  object-fit: cover;
+}
+
 .delete-file {
   position: absolute;
-  top: 1px;
-  right: 5px;
-  font-size: 14px;
+  top: 5px;
+  right: 8px;
+  cursor: pointer;
 }
-.name {
-  width: 100%;
+
+.select-pages {
   position: absolute;
-  bottom: 2px;
+  bottom: 30px;
+  left: 0;
+  right: 0;
+  font-size: 10px;
+}
+
+.name {
+  width: 80%;
+  margin: auto;
   font-size: 10px;
   text-overflow: ellipsis;
   white-space: nowrap;
   overflow: hidden;
-  padding: 0 2px;
+  padding: 2px;
 }
 
-.icon {
-  height: 70%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.pages {
-  width: 85%;
+input {
+  width: 80%;
   height: 10px;
   margin: 0 auto;
-  border: 0.5px solid #000;
+  border: 0.5px solid #e5e7eb;
   border-radius: 3px;
   padding: 4px;
+}
+
+input:focus {
+  outline: none;
 }
 </style>
---
+++ src/pdfToImg.js
@@ -0,0 +1,30 @@
+import * as pdfjsLib from 'pdfjs-dist';
+import * as pdfjsWorker from "pdfjs-dist/build/pdf.worker";
+
+export default async function pdfToImg(pdfFile) {
+  const reader = new FileReader();
+  reader.readAsArrayBuffer(pdfFile);
+  const data = await new Promise((resolve) => {
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+  });
+  const pdfData = new Uint8Array(data);
+
+  //set Worker
+  pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
+  const pdf = await pdfjsLib.getDocument(pdfData).promise;
+  const page = await pdf.getPage(1);
+  const viewport = page.getViewport({ scale: 0.5 });
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+  const renderContext = {
+    canvasContext: context,
+    viewport: viewport
+  };
+  await page.render(renderContext).promise;
+  return canvas.toDataURL();
+}
